f7212724431062104dca36d52694d766e71d7660
Daniel <danimontesinos1@gmail.com>
2026-01-18 22:07:13 +0100

    Changes

--- test_slack_manual.py
+++ test_slack_manual.py
@@ -4,7 +4,7 @@ from notification_hub import NotificationFactory
 
 # Configuration
 # You can set the SLACK_BOT_TOKEN environment variable or edit this file directly
-SLACK_TOKEN = os.environ.get("SLACK_BOT_TOKEN", "xoxb-xxx")
+SLACK_TOKEN = os.environ.get("SLACK_BOT_TOKEN", "TOKEN_HERE")
 CHANNEL = "#automations" # Change this to the channel you want to test (ensure the bot is in the channel)
 
 def main():
